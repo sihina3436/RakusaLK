cfb8d2c40da4bcda4ea96f9845e8a2e349990636
H.K.Sihina Nimnada <nimnadasihina@gmail.com>
2026-05-06 22:48:07 +0530

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://rakusa-lk-d6e9.vercel.app', credentials: true }));
 
 // MongoDB
 mongoose
